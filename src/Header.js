b808679511d7e3562bef17696dee86c932493dac
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-10 00:13:14 +0300

    header section is done

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { IconButton } from "@mui/material";
+import { Avatar, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 import "./Header.css";
 
@@ -13,8 +17,20 @@ function Header() {
         </IconButton>
         <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt=""/>
       </div>
-      <div className="header__middle"></div>
-      <div className="header__right"></div>
+      <div className="header__middle">
+        <SearchIcon/>
+        <input placeholder="search mail" type="test"/>
+        <ArrowDropDownIcon/>
+      </div>
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon/>
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon/>
+        </IconButton>
+        <Avatar/>
+      </div>
     </div>
   );
 }
