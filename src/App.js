8dcf8267dd2e308108cc438d51d6c29cc4152621
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-10 08:29:28 +0300

    Header and Sidebar are now in place

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
-      <h2>Let's build gmail</h2>
+      <Header />
+      <div className="app__boddy">
+        <Sidebar />
+      </div>
     </div>
   );
 }
