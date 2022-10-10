c4bec431bdad08e4c9ee813f7cf2ac678737c373
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-10 08:42:05 +0300

    added sidebar footer and its styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
 import InboxIcon from "@mui/icons-material/Inbox";
@@ -8,6 +8,9 @@ import LabelImportantIcon from '@mui/icons-material/LabelImportant';
 import NearMeIcon from '@mui/icons-material/NearMe';
 import NoteIcon from '@mui/icons-material/Note';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import PersonIcon from '@mui/icons-material/Person';
+import DuoIcon from '@mui/icons-material/Duo';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 import "./Sidebar.css";
 import SidebarOption from "./components/SidebarOption";
@@ -34,6 +37,14 @@ function Sidebar() {
       <SidebarOption Icon={NearMeIcon} title="Sent" number={54} />
       <SidebarOption Icon={NoteIcon} title="Drafts" number={54} />
       <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+            <IconButton><PersonIcon/></IconButton>
+            <IconButton><DuoIcon/></IconButton>
+            <IconButton><PhoneIcon/></IconButton>
+        </div>
+      </div>
     </div>
   );
 }
