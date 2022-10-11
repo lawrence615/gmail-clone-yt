94bc5ff1826f8f6bb5987ce9975f9990362803e3
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-11 06:08:42 +0300

    The middle section is using router to render the pages/views

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
-import "./App.css";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import List from "./mail/List";
+import Mail from "./mail/Mail";
+import "./App.css";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <List />,
+    },
+    {
+      path: "/mail",
+      element: <Mail />,
+    },
+  ]);
+
   return (
     <div className="app">
       <Header />
-      <div className="app__boddy">
+      <div className="app__body">
         <Sidebar />
+        <React.StrictMode>
+          <RouterProvider router={router} />
+        </React.StrictMode>
       </div>
     </div>
   );
