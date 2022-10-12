4270e178c5a200ac1f6ba3614664ea70c48ecce7
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-12 05:37:10 +0300

    Added the tabs and icons on the List page. The styling also

--- src/mail/List.js
+++ src/mail/List.js
@@ -1,12 +1,62 @@
-import React from 'react'
+import React from "react";
+import { Checkbox, IconButton } from "@mui/material";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import RedoIcon from "@mui/icons-material/Redo";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import KeyboardHideIcon from "@mui/icons-material/KeyboardHide";
+import SettingsIcon from "@mui/icons-material/Settings";
+import InboxIcon from "@mui/icons-material/Inbox";
+import PeopleIcon from "@mui/icons-material/People";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 
-
-import './List.css'
+import "./List.css";
+import Section from "../components/Section";
+import Row from "../components/Row";
 
 function List() {
   return (
-    <div className='emailList'>List</div>
-  )
+    <div className="emailList">
+      <div className="emailList__settings">
+        <div className="emailList__settingsLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDownwardIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="emailList__sections">
+        <Section Icon={InboxIcon} title="primary" color="red" selected />
+        <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
+        <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+      </div>
+      <div className="emailList__list">
+        <Row title='Twitch' subject='Hey fellow streamer!!!' description='This is a test.' time='10pm'/>
+        <Row title='Twitch' subject='Hey fellow streamer!!!' description='This is a test.' time='10pm'/>
+      </div>
+    </div>
+  );
 }
 
-export default List
+export default List;
