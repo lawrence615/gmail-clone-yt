961490660e997339dc9ac6997f8f09ae3965d489
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-12 17:43:43 +0300

    styling of the Mail view page

--- src/mail/Mail.js
+++ src/mail/Mail.js
@@ -1,12 +1,79 @@
-import React from 'react'
+import React from "react";
+import { IconButton } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import MoveToInboxIcon from "@mui/icons-material/MoveToInbox";
+import ErrorIcon from "@mui/icons-material/Error";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EmailIcon from "@mui/icons-material/Email";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import UnfoldMoreIcon from "@mui/icons-material/UnfoldMore";
+import PrintIcon from "@mui/icons-material/Print";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import { useNavigate } from "react-router-dom";
 
-
-import './Mail.css'
+import "./Mail.css";
 
 function Mail() {
+  const nav = useNavigate();
+
   return (
-    <div className='mail'>Mail</div>
-  )
+    <div className="mail">
+      <div className="mail__tools">
+        <div className="mail__toolsLeft">
+          <IconButton onClick={() => nav("/")}>
+            <ArrowBackIcon />
+          </IconButton>
+          <IconButton>
+            <MoveToInboxIcon />
+          </IconButton>
+          <IconButton>
+            <ErrorIcon />
+          </IconButton>
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton>
+            <EmailIcon />
+          </IconButton>
+          <IconButton>
+            <WatchLaterIcon />
+          </IconButton>
+          <IconButton>
+            <CheckCircleIcon />
+          </IconButton>
+          <IconButton>
+            <LabelImportantIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="mail__toolsRight">
+          <IconButton>
+            <UnfoldMoreIcon />
+          </IconButton>
+          <IconButton>
+            <PrintIcon />
+          </IconButton>
+          <IconButton>
+            <ExitToAppIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="mail__body">
+        <div className="mail__bodyHeader">
+          <h2>Subject</h2>
+          <LabelImportantIcon className="mail__important"/>
+          <p>Title</p>
+          <p className="mail__time">10pm</p>
+        </div>
+        <div className="mail__message"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
+      </div>
+    </div>
+  );
 }
 
-export default Mail
+export default Mail;
