4997ef3637425ff151702c5a7bdb0288a82785d1
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-13 08:52:22 +0300

    The compose dialog has been fully implemented; functionality and styling

--- src/components/SendMail.js
+++ src/components/SendMail.js
@@ -1,15 +1,62 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
+import CloseIcon from "@mui/icons-material/Close";
+import { Button } from "@mui/material";
+import { useForm } from "react-hook-form";
 
-import './SendMail.css'
+import { closeSendMessage } from "../features/mailSlice";
+import "./SendMail.css";
+
+
 
 function SendMail() {
+  const dispatch = useDispatch();
+  const { register, handleSubmit, watch, formState:{ errors } } = useForm();
+
+  const onSubmit = (formData) => {
+    console.log("onSubmit", formData);
+  };
+
   return (
-    <div className='sendMail'>
-        <div className='sendMail__header'>
-            
+    <div className="sendMail">
+      <div className="sendMail__header">
+        <h3>New Message</h3>
+        <CloseIcon onClick={()=>dispatch(closeSendMessage())} className="sendMail__close" />
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="To"
+          type="email"
+          {...register("to", { required: true })}
+        />
+        {errors.to && <p className="sendMail__error">To is required!</p>}
+        <input
+          placeholder="Subject"
+          type="text"
+          {...register("subject", { required: true })}
+          aria-invalid={errors.subject ? "true" : "false"}
+        />
+        {errors.subject && <p className="sendMail__error">Subject is required!</p>}
+        <input
+          placeholder="Message..."
+          type="text"
+          className="sendMail__message"
+          {...register("message", { required: true })}
+        />
+        {errors.message && <p className="sendMail__error">Message is required!</p>}
+        <div className="sendMail__options">
+          <Button
+            className="sendMail__send"
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Send
+          </Button>
         </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SendMail
+export default SendMail;
