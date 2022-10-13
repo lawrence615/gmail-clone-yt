51a54ca3b162413ee2b0395615a531add51cbf0d
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-13 03:55:20 +0300

    initial commit - creating an email compose component

---
+++ src/components/SendMail.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './SendMail.css'
+
+function SendMail() {
+  return (
+    <div className='sendMail'>
+        <div className='sendMail__header'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default SendMail
