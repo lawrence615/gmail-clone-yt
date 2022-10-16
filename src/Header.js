65a91c5afa57702f69ec20e031e904f23b254a4e
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-16 04:32:10 +0300

    displaying the current user on the avatar; and user can also sign out when same is clicked

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Avatar, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
@@ -6,9 +7,27 @@ import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import AppsIcon from "@mui/icons-material/Apps";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 
+import { logout, selectUser } from "./features/userSlice";
 import "./Header.css";
+import { auth, signOut } from "./services/firebase";
 
 function Header() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  console.log(user.photoUrl);
+
+  const onSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        dispatch(logout);
+      })
+      .catch((error) => {
+        // An error happened.
+        alert(error);
+      });
+  };
+
   return (
     <div className="header">
       <div className="header__left">
@@ -32,7 +51,9 @@ function Header() {
         <IconButton>
           <NotificationsIcon />
         </IconButton>
-        <Avatar />
+        <Avatar onClick={onSignOut} src={user?.photoUrl}>
+          {user?.displayName[0]}
+        </Avatar>
       </div>
     </div>
   );
